9a19a5d5a096693d22861b7ac0a7ed9d6e85c7c8
aniketghavte <104782783+aniketghavte@users.noreply.github.com>
2023-04-12 12:40:03 +0530

    desktop and tablet ready

--- src/app/page.js
+++ src/app/page.js
@@ -1,102 +1,46 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import waitlist_pic from '../../public/waitlistpic.svg';
+import line from '../../public/line.svg'
+import getStarted from '../../public/getStarted.svg'
+import joinWaitlistImage from '../../public/joinWaitlistImage.svg'
+import finalImage from '../../public/finalImage.svg'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className={styles.app__main}>
+        <div className={styles.block1}>
+           <div>
+           <Link href="/"><p>ActualOne Protocol</p></Link>
+           </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <div className={styles.block2}>
+            <div className={styles.headline}>
+               ActualOne Protocol is coming soon ⚡
+            </div>
+            <div className={styles.mainHeadline}>
+              <p>Join&nbsp; 
+                <span className={styles.waitlist_pic}>
+                <Image src={waitlist_pic} alt="waitlist " className={styles.span_image}/>
+                <Image src={line} alt="line " className={styles.lineImage}/>
+                  Wetlist&nbsp;
+                </span> 
+                  & Get notified when we&nbsp;
+                <span className={styles.launch_span}> 
+                  Launch
+                </span>. 🚀</p>
+                <Image src={getStarted} alt="get Started " className={styles.span_image}/>
+            </div>
+            <div className={styles.block3}>
+               <input type="text" placeholder="What's your email"/>
+               <button>Subscribe</button>
+               <Image src={joinWaitlistImage} alt="get Started " className={styles.joinWaitlistImage}/>
+            </div>
+            
+            <Image src={finalImage} alt="get Started " className={styles.finalImage}/>
+        </div>
     </main>
   )
 }
